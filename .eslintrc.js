a9b99fd7afb7e9c9815f30c227f17331751c6c81
TheAmniel <luismartz199@gmail.com>
2022-05-14 10:54:06 -0400

    Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: false,
+    commonjs: true,
+    es6: false,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  ignorePatterns: [ "testing/*" ],
+  parserOptions: {
+    sourceType: 'commonjs',
+    ecmaFeatures: {
+      jsx: false,
+    },
+    ecmaVersion: 11,
+  },
+  rules: {
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "array-bracket-spacing": 'error',
+    "arrow-spacing": 'error',
+    complexity: 'error',
+    curly: 'off',
+    'no-buffer-constructor': 'error',
+    'no-var': 'error'
+  },
+};
