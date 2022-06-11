21017ddde306c6d152eca69292060af3b62161db
davidsdevel <djgm1206@gmail.com>
2022-06-11 03:22:04 -0430

    Mergedwith remote

--- davidsdevel-accounts/lib/login.js
+++ davidsdevel-accounts/lib/login.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const {accounts} = require(process.cwd() + '/mongo');
-=======
 const {accounts} = require('@lettercms/models');
->>>>>>> 6baba5a4ede63f76da4bb88754918282eebfd2dc
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS = join(process.cwd(), 'davidsdevel-accounts', 'firebaseAdmin.json');
 
