77580336d8f430fbd1f9051931620215efb47451
davidsdevel <djgm1206@gmail.com>
2022-07-25 18:50:45 -0430

    Init recommendation support

--- davidsdevel-posts/lib/url.patch.js
+++ davidsdevel-posts/lib/url.patch.js
@@ -1,5 +1,6 @@
-const {posts} = require('@lettercms/models');
+const {posts, users: {Ratings, Users}} = require('@lettercms/models');
 const {isValidObjectId} = require('mongoose');
+const brain = require('../../brain');
 
 module.exports = async function() {
   const {req, res} = this;
@@ -54,13 +55,48 @@ module.exports = async function() {
       });
   }
 
+  if (action === 'draft') {
+    Ratings.deleteMany({post: data.id});
+  }
+
   if (data.exists)
     return res.json({
       message: 'Url Exists'
     });
 
+  if (action === 'publish' && !data.exists) {
+    Users.find({subdomain}, '_id hasRecommendations mlModel')
+      .then(users => {
+        Promise.allSettled(
+          users.map(({_id, hasRecommendations, mlModel}) => {
+            let rating = 0;
+            if (mlModel) {
+              const parsedTags = {};
+
+              data.tags.forEach(e => parsedTags[e] = 1);
+              
+              const cross = new brain.CrossValidate(brain.NeuralNetwork);
+
+              const net = cross.fromJSON(JSON.parse(mlModel));
+
+              rating = net.run(parsedTags);
+            }
+
+            return Ratings.create({
+              userID: _id,
+              post: data.id,
+              subdomain,
+              rating
+            });
+          })
+        ).then(() => {
+          delete data.tags;
+        })
+      });
+  }
+
   res.json({
     status: 'OK',
-    data
+    id: data.id
   });
 };
--- davidsdevel-users/lib/recomendation.post.js
+++ davidsdevel-users/lib/recomendation.post.js
@@ -1,4 +1,8 @@
 const {posts, users: {Users, Ratings}} = require('@lettercms/models');
+const jwt = require('jsonwebtoken');
+const fetch = require('node-fetch');
+
+const endpoint = process.env.NODE_ENV === 'production' ? 'https://lettercms-recommendation.herokuapp.com' : 'http://localhost:3008';
 
 const parseTags = arr => {
   const tags = {};
@@ -33,8 +37,30 @@ module.exports = async function() {
   const {tags, _id} = await posts.findOne({subdomain, url}, 'tags');
 
   const views = parseTags(tags);
-  const {postsView} = await Users.findOneAndUpdate({_id: id}, {$push: {views}, $inc: {postsView: 1}});
+  const {postsView} = await Users.findOne({_id: id}, 'postsView', {lean: true});
+
+  if (postsView < 5)
+    await Users.updateOne({_id: id}, {views: [
+      views,
+      views,
+      views,
+      views,
+      views,
+    ], postsView: 5});
+  else
+    await Users.updateOne({_id: id}, {$push: {views}, $inc: {postsView: 1}});
+
+
   await Ratings.updateOne({post: _id}, {viewed: true});
+
+  const token = jwt.sign({subdomain}, process.env.JWT_AUTH);
+
+  fetch(`${endpoint}/api/recommendation/${id}`, {
+    method: 'POST',
+    headers: {
+      Authorization: token
+    }
+  })
 
   res.json({
     status: 'OK'
