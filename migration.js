9b657c593a04ea07373f980cbdb3dde938e0eba5
davidsdevel <djgm1206@gmail.com>
2022-07-31 08:05:47 -0430

    Migration

--- migration.js
+++ migration.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 //Migrar posts
 
 /**
@@ -8,25 +10,40 @@
  */
 
  module.exports = async function() {
+   const m = await mongoose.createConnection(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
+
+  const {posts, users: {Users}, accounts: {Accounts}} = require('@lettercms/models')(m, ['posts', 'accounts', 'users']);
+
+
   //Migrating postsSchema
   const r = await posts.find({authorEmail: {$exists: true}}, 'authorEmail', {lean: true});
   
-  await Promise.allSettled(r.map(async e => {
+  const rr = await Promise.allSettled(r.map(async e => {
     const {_id} = await Accounts.findOne({email: e.authorEmail});
 
     return posts.updateMany({authorEmail: e.authorEmail}, {author: _id, $unset: {authorEmail:1}});
   }));
 
+  console.log(rr)
+
 
   const r2 = await posts.find({authorEmail: {$exists: false}}, 'subdomain', {lean: true});
-  await Promise.allSettled(r2.map(async e => {
+  const r2r = await Promise.allSettled(r2.map(async e => {
     const {ownerEmail} = await blogs.findOne({subdomain: e.subdomain}, 'ownerEmail', {lean: true});
     const {_id} = await Accounts.findOne({email: ownerEmail}, '_id', {lean: true});
 
     return posts.updateMany({subdomain: e.subdomain, authorEmail: {$exists: false}}, {author: _id, $unset: {authorEmail:true}});
   }));
 
+  console.log(r2r);
+ 
+  const u = Users.updateMany({}, {$unset: {email: 1}});
 
-  
+  console.log(u);
  };
  
