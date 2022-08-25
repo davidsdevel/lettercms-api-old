19364cbfc3f83128cb6cfdda9f0519ea56862d23
davidsdevel <djgm1206@gmail.com>
2022-08-25 08:03:21 -0400

    Removed unused api

--- davidsdevel-admins/api/nextjs/blogs.js
+++
@@ -1,7 +0,0 @@
-const {blogs} = require('@lettercms/models')(["blogs"])
-
-module.exports = async function(req, res) {
-  const data = await blogs.find({}, "subdomain", {lean: true});
-
-  res.json(data.map(e => e.subdomain));
-}
