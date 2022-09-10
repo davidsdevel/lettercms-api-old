5983c170156651b5564c36e141b9eca46d3af9bc
davidsdevel <djgm1206@gmail.com>
2022-09-09 22:05:35 -0400

    Updated generations

--- generateManifest.js
+++ generateManifest.js
@@ -7,7 +7,11 @@ const routes = mapR(routesPath);
 
 console.log(routes)
 
-const mapped = Object.values(routes).flat().sort(e => e.url.includes(':') ? +1 : -1);
+const mapped = Object.values(routes).flat().map(e => {
+  e.file = e.file.replace(/.*path0/' ..');
+
+  return e;
+}).sort(e => e.url.includes(':') ? +1 : -1);
 
 appendFile('./manifest.json', JSON.stringify(mapped), err => {
   if (err)
